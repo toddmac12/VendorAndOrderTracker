d10e1a87e3d8af3a7d06125b2a1f43d3c444f63c
toddmac12 <toddmac12@gmail.com>
2021-10-01 16:53:38 -0400

    test GetAll Vendors passed.

--- VendorAndOrderTracker.Test/ModelTests/VendorTest.cs
+++ VendorAndOrderTracker.Test/ModelTests/VendorTest.cs
@@ -43,23 +43,36 @@ namespace VendorAndOrderTracker.Tests
       Assert.AreEqual(result, description);
 
     }
- [TestMethod]
+    [TestMethod]
     public void SetName_SetName_String()
     {
-      Vendor newVendor = new Vendor("testName","testDescript");
+      Vendor newVendor = new Vendor("testName", "testDescript");
       string updatedDescript = "updatedDescript";
       newVendor.Description = updatedDescript;
       string result = newVendor.Description;
-      Assert.AreEqual(result,updatedDescript);
+      Assert.AreEqual(result, updatedDescript);
     }
-[TestMethod]
+    [TestMethod]
     public void SetDescription_SetSetDescription_String()
     {
-      Vendor newVendor = new Vendor("testName","testDescript");
+      Vendor newVendor = new Vendor("testName", "testDescript");
       string updatedDescript = "updatedDescript";
       newVendor.Description = updatedDescript;
       string result = newVendor.Description;
-      Assert.AreEqual(result,updatedDescript);
+      Assert.AreEqual(result, updatedDescript);
+    }
+    [TestMethod]
+    public void GetAll_ReturnVendors_VendorList()
+    {
+      string name1 = "vendorname1";
+      string name2 = "vendorname2";
+      string description01 = "descript1";
+      string description02 = "descript2";
+      Vendor newVendor1 = new Vendor(name1, description01);
+      Vendor newVendor2 = new Vendor(name2, description02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -1,26 +1,32 @@
 using System.Collections.Generic;
 
 namespace VendorAndOrderTracker.Models
-{public class Vendor{
-public string VendorName {get;set;}
-public int Id{get;}
-
-public string Description {get;set;}
-private static List<Vendor> _instances = new List<Vendor>{ };
-
-public Vendor(string vendorName, string description)
 {
-VendorName = vendorName;
-Description = description;
-_instances.Add(this);
-Id=_instances.Count;
+  public class Vendor
+  {
+    public string VendorName { get; set; }
+    public int Id { get; }
+
+    public string Description { get; set; }
+    private static List<Vendor> _instances = new List<Vendor> { };
+
+    public Vendor(string vendorName, string description)
+    {
+      VendorName = vendorName;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+public static List<Vendor>GetAll()
+{
+return _instances;
 }
 
-public static void ClearAll()
-{
-_instances.Clear();
-}
-}
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
 }
 
 
