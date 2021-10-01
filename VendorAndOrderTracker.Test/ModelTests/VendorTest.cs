89e04e28be66567c688715c17858b4b6fd50b1c0
toddmac12 <toddmac12@gmail.com>
2021-10-01 14:31:30 -0400

    test created for vendor model. First test failed. Contructor for Vendor name created and test passed.

--- VendorAndOrderTracker.Test/ModelTests/VendorTest.cs
+++ VendorAndOrderTracker.Test/ModelTests/VendorTest.cs
@@ -1,3 +1,27 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using VendorAndOrderTracker.Models;
+using System;
+
+namespace VendorAndOrderTracker.Tests
+
+{
+
+  [TestClass]
+  public class VendorTests
+{
+[TestMethod]
+  public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+  {
+    Vendor newVendor = new Vendor("testName", "testDescript");
+    Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+
+}
+}
+}
+
+
+
+
+
--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -1,1 +1,23 @@
 using System.Collections.Generic;
+
+namespace VendorAndOrderTracker.Models
+{public class Vendor{
+public string VendorName {get;set;}
+public int Id{get;}
+
+public string Description {get;set;}
+private static List<Vendor> _instances = new List<Vendor>{ };
+
+public Vendor(string vendorName, string description)
+{
+VendorName = vendorName;
+Description = description;
+_instances.Add(this);
+Id=_instances.Count;
+}
+}
+}
+
+
+
+
