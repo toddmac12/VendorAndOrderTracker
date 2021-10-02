d68dd8896308a95944b3a4c1cdd57f543ab30248
toddmac12 <toddmac12@gmail.com>
2021-10-01 20:50:45 -0400

    vendor order Return orders passed.

--- VendorAndOrderTracker.Test/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Test/ModelTests/OrderTests.cs
@@ -35,7 +35,28 @@ namespace VendorAndOrderTracker.Tests.ModelTests
       string result = newOrder.Description;
       Assert.AreEqual(result, description);
     }
-
+    [TestMethod]
+    public void SetTitle_SetTitle_String()
+    {
+      Order newOrder = new Order("testtitle", "testDescription", 0, "testdate");
+      string updatedTitle = "updatedTitle";
+      newOrder.Title = updatedTitle;
+      string result = newOrder.Title;
+      Assert.AreEqual(result, updatedTitle);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      string Title1 = "order1";
+      string description1 = "orderdescript1";
+      string Title2 = "order2";
+      string description2 = "order01descript2";
+      Order newOrder1 = new Order(Title1, description1, 0, "testDate");
+      Order newOrder2 = new Order(Title2, description2, 0, "testDate");
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
 
